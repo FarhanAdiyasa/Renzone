cb8b5a94a330d911e5b170f19be12006d1f65797
KhairunnisaAB <khrnnisa.bsr14@gmail.com>
2023-06-22 14:45:17 +0700

    tambah edmx renzone

--- Renzone/AddCategory.designer.cs
+++ Renzone/AddCategory.designer.cs
@@ -50,6 +50,7 @@
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(209, 129);
             this.flowLayoutPanel2.TabIndex = 0;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
             // 
             // lbCategory
             // 
